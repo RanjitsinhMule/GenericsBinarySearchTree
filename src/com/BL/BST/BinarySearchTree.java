bdb434dfb16ceb0e5ae5c8347bd50be3f8e787d7
RanjitsinhMule <muleranjitsinh7323@gmail.com>
2022-09-22 16:02:42 +0530

    UC2 added

--- src/com/BL/BST/BinarySearchTree.java
+++ src/com/BL/BST/BinarySearchTree.java
@@ -1,43 +1,49 @@
 package com.BL.BST;
 
 class Node {
-	int data;
-	Node left;
-	Node right;
-}
+	 int data;
+     Node left;
+     Node right;
+ }
+ // All methods are nonstatic in the class so create object for this class in main method
+ public class BinarySearchTree {
+     //insert method is used here to insert root and value
+     public Node insert(Node root, int val) {
+         if (root == null) {
+             return createNewNode(val);
+         }if (val < root.data) {
+             root.left = insert(root.left, val);
+         } else if (val > root.data) {
+             root.right = insert(root.right, val);
+         }
+         return root;
+     }
+     public Node createNewNode(int k) {//Create method createNewNode
+         Node bst = new Node();
+         bst.data = k;
+         bst.left = null;
+         bst.right = null;
+         return bst;
+     }
 
-public class BinarySearchTree {
-	// insert method is used here to insert root and value
-			public Node insert(Node root, int val) {
-
-				if (root == null) {
-					return createNewNode(val);
-				}
-				if (val < root.data) {
-					root.left = insert(root.left, val);
-
-				} else if (val > root.data) {
-					root.right = insert(root.right, val);
-				}
-				return root;
-			}
-
-			public Node createNewNode(int k) {// Create method createNewNode
-				Node bst = new Node();
-				bst.data = k;
-				bst.left = null;
-				bst.right = null;
-				return bst;
-			}
-
-			public void print(Node root) {// Here print method is created to print the data
-				if (root == null) {
-					return;
-				}
-				print(root.left);
-				System.out.print(root.data + "->");
-				// null right side
-				print(root.right);
-			}
+     public void print(Node root) {//Here print method is created to print the data
+         if (root == null) {
+             return;
+         }
+         print(root.left);
+         System.out.print(root.data +"->");
+         print(root.right);
+     }
+     //Size method For checking all nodes are added or Not
+     public int size(Node node) {
+         /*
+          *  if condition is true then node is empty if condition is false then
+          * else condition is worked
+          */
+         if (node == null)
+             return 0;
+         else
+             return(size(node.left) + 1 + size(node.right));
+     }
 		
 }
--- src/com/BL/BST/BinarySearchTreeMain.java
+++ src/com/BL/BST/BinarySearchTreeMain.java
@@ -2,17 +2,24 @@ package com.BL.BST;
 
 public class BinarySearchTreeMain {
 	public static void main(String[] args) {
-        System.out.println("Welcome to Binary Search Tree" +
-                 "program");
+        System.out.println("Welcome to Binary Search Tree Program");
 
         BinarySearchTree bst = new BinarySearchTree();
         Node root = null;
         // inserting the value in root
         root = bst.insert(root, 56); // 56 is adding in root
-
         root = bst.insert(root, 30);
-
         root = bst.insert(root, 70);
+        root = bst.insert(root, 22);
+        root = bst.insert(root, 40);
+        root = bst.insert(root, 60);
+        root = bst.insert(root, 95);
+        root = bst.insert(root, 11);
+        root = bst.insert(root, 65);
+        root = bst.insert(root, 3);
+        root = bst.insert(root, 6);
+        root = bst.insert(root, 63);
+        root = bst.insert(root, 67);
 
         bst.print(root);
 
